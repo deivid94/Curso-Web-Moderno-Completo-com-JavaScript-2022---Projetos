04fcece9f9c199b0d800aea3210a8aa922cf0ec2
Deivid Martins <md031194@gmail.com>
2023-03-15 00:27:50 -0300

    Algoritimo de comparacao de dois arrays

--- algoritimos/index.js
+++ algoritimos/index.js
@@ -1,23 +1,32 @@
-const { edFolha, edGalho} = require('./arrays')
+const { edFolha, edGalho } = require('./arrays')
 
-function juntaLista (lista1, lista2){
-    let atual = 0;
-    let listafinal = [];
-    let posicaoAtualLista1 = 0;
-    let posicaoAtualLista2 = 0;
+
+
+ function juntaLista (lista1, lista2){
+     let atual = 0;
+     let listafinal = [];
+     let posicaoAtualLista1 = 0;
+     let posicaoAtualLista2 = 0;
     
 
-    while (posicaoAtualLista1 < lista1.lenght && posicaoAtualLista2 < lista2.lenght){
-
-    let produtoLista1 = lista1[posicaoAtualLista1];
-    let produtoLista2 = lista2[posicaoAtualLista2];
-
-    if (produtoLista1.preco < produtoLista2.preco){
-
-    }
-
-
+     while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length){
+        let produtoAtualLista1 = lista1[posicaoAtualLista1]
+        let produtoAtualLista2 = lista2[posicaoAtualLista2]
+        
+     
+     if (produtoAtualLista1.preco < produtoAtualLista2.preco){
+         listafinal[atual] = produtoAtualLista1
+        posicaoAtualLista1++
+       
+     }else {
+        listafinal[atual] = produtoAtualLista2
+        posicaoAtualLista2++
+     }
+     atual++
 
     }
     return listafinal
 }
+
+console.log(juntaLista(edFolha, edGalho))
+     
