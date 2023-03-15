b37654f6a7fa10d47630c141090af7b38894b43c
Deivid Martins <md031194@gmail.com>
2023-03-15 00:26:55 -0300

    Algoritmo de mergeSorte (mais usual no dia a dia)

---
+++ algoritimos/mergeSort.js
@@ -0,0 +1,12 @@
+const listaLivros = require('./array2');
+
+
+function mergeSorte (array){
+    if (array < array.length){
+        let meio 
+        let parte1 = Math.floor((array.length / 2)) 
+        let parte2 = 
+
+    }
+}
+
