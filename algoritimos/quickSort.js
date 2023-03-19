2a2dd01aac30378342cbcba2581491e2a1a3d0cb
Deivid Martins <md031194@gmail.com>
2023-03-19 18:28:25 -0300

    Criando algoritimo quicksort

---
+++ algoritimos/quickSort.js
@@ -0,0 +1,27 @@
+const listaLivros = require  ('./array2')
+console.log(listaLivros)
+
+
+function encontraMenores (pivo, array){
+    let menores = 0
+    
+    for (let atual = 0; atual < array.length; atual++){
+        let produtoAtual = array[atual]
+        if (produtoAtual.preco < pivo.preco){
+            menores++
+        }
+    }
+    trocaLugar(array, array.indexOf(pivo), menores)
+    return array;
+}
+
+function trocaLugar(array, de , para){
+    const  elemento1 = array[de];
+    const  elemento2 = array[para];
+
+    array[para] = elemento1
+    array[de] = elemento2
+
+}
+
+console.log(encontraMenores(listaLivros[2], listaLivros))
